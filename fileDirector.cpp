791fa231ca4139489671de0bbadbe517170ae6f6
FilipCysEk <krzysiek.swal@gmail.com>
2019-03-24 18:24:02 +0100

    Now app change name, when in printed folder find existing

--- fileDirector.cpp
+++ fileDirector.cpp
@@ -19,6 +19,8 @@ private:
 
 	void prepareDirs();
 	bool isPdf(char*);
+	bool isNum(char);
+	string repetableFileName(string);
 public:
 	FileDirector(string);
 	vector<string> scanPrintedDir();
@@ -56,14 +58,20 @@ vector<string> FileDirector::scanPrintedDir(){
 		vector<string> pdfList;
 		while((ent = readdir(pdfsDir)) != NULL){
 			if(isPdf(ent->d_name)){
-				pdfList.push_back(ent->d_name);
-				fs::rename((path + '/' + ent->d_name).c_str(), (path + "/printed/" + ent->d_name).c_str(),errFS);
+				if(fs::exists((path + "/printed/" + ent->d_name).c_str())){
+					pdfList.push_back(repetableFileName(ent->d_name));
+				}
+				else{
+					pdfList.push_back(ent->d_name);
+				}
+
+				fs::rename((path + '/' + ent->d_name).c_str(), (path + "/printed/" + pdfList.back()).c_str(),errFS);
 
 				if( errFS.value() != 0){
 					logTfile.addNewLineL1("Error when copying file " + errFS.message() + ". App closed!");
 					exit(3);
 				}
-				logTfile.addNewLineL3("Copied file: " + (string)(ent->d_name));
+				logTfile.addNewLineL3("Copied file: " + (string)(ent->d_name) + " as " + pdfList.back());
 			}
 		}
 
@@ -81,5 +89,43 @@ bool FileDirector::isPdf(char* fname){
 		if(fname[len-1] == 'f' && fname[len-2] == 'd' && fname[len-3] == 'p' && fname[len-4] == '.')
 			return true;
 	}
+	return false;
+}
+
+string FileDirector::repetableFileName(string name){
+	string namecut[3];
+	int len, i;
+	while(fs::exists((path + "/printed/" + name).c_str())){
+		len = name.length();
+		namecut[2] = name.substr( len - 4);
+		i = len - 4;
+		for(--i; i >= 0; i--){
+			if(!isNum(name[i])){
+				break;
+			}
+		}
+
+		namecut[0] = name.substr(0, i + 1);
+		if(len - 4 - i - 1 > 0){
+			namecut[1] = name.substr(i + 1, len - 4 - i - 1);
+			//i now is temp;
+			i = stoi(namecut[1]);
+			i++;
+			namecut[1] = to_string(i);
+
+			name = namecut[0] + namecut[1] + namecut[2];
+		}
+		else
+			name = namecut[0] + '0' + namecut[2];
+
+		//logTfile.addNewLineL3("file: " + namecut[0] + " - " + namecut[1] + " -- " + namecut[2]);
+	}
+	return name;
+}
+
+bool FileDirector::isNum(char ch){
+	if(ch >= '0' && ch <= '9')
+		return true;
+
 	return false;
 }
